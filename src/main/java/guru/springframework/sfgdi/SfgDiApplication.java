f03fb95844eae7c7d9881b34472e702f23067dca
Richard Salac <richard.salac@gmail.com>
2021-09-24 10:34:42 +0200

    Fix assignment merge

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -3,6 +3,7 @@ package guru.springframework.sfgdi;
 import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
 import guru.springframework.sfgdi.controllers.I18nController;
 import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PetController;
 import guru.springframework.sfgdi.controllers.PropertyInjectedController;
 import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import java.util.stream.Stream;
@@ -17,7 +18,11 @@ public class SfgDiApplication {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
                 
                 //Stream.of(ctx.getBeanDefinitionNames()).sorted().forEach(System.out::println);
-                 
+               
+                PetController petController = ctx.getBean("petController", PetController.class);
+		System.out.println("--- The Best Pet is ---");
+		System.out.println(petController.whichPetIsTheBest());
+                
                 I18nController i18nController = (I18nController) ctx.getBean("i18nController");
                 System.out.println(i18nController.sayHello());
                 
