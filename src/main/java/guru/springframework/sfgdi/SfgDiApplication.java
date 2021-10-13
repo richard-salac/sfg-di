44300cbe67989bbd499a666d739bb8268c0ee7f5
Richard Salac <richard.salac@gmail.com>
2021-10-13 14:26:26 +0200

    Section 6 - 95. Properties Binding

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,11 +1,13 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.config.SfgConfiguration;
 import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
 import guru.springframework.sfgdi.controllers.I18nController;
 import guru.springframework.sfgdi.controllers.MyController;
 import guru.springframework.sfgdi.controllers.PetController;
 import guru.springframework.sfgdi.controllers.PropertyInjectedController;
 import guru.springframework.sfgdi.controllers.SetterInjectedController;
+
 import java.util.stream.Stream;
 
 import guru.springframework.sfgdi.datasource.FakeDataSource;
@@ -15,48 +17,55 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"guru.springframework.sfgdi","guru.springframework.pets"})
+@ComponentScan(basePackages = {"guru.springframework.sfgdi", "guru.springframework.pets"})
 public class SfgDiApplication {
 
-	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
-                
-                //Stream.of(ctx.getBeanDefinitionNames()).sorted().forEach(System.out::println);
-               
-                PetController petController = ctx.getBean("petController", PetController.class);
-		System.out.println("--- The Best Pet is ---");
-		System.out.println(petController.whichPetIsTheBest());
-                
-                I18nController i18nController = (I18nController) ctx.getBean("i18nController");
-                System.out.println(i18nController.sayHello());
-                
-                System.out.println("------------ Primary");
-                
-                MyController myController = (MyController) ctx.getBean("myController");
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
-                System.out.println(myController.sayHello());
-                
-                System.out.println("------------ Property");
-                
-                PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-                
-                System.out.println(propertyInjectedController.getGreeting());
-               
-                System.out.println("------------ Setter");
-                
-                SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean ("setterInjectedController");
-               
-                System.out.println(setterInjectedController.getGreeting());
+        //Stream.of(ctx.getBeanDefinitionNames()).sorted().forEach(System.out::println);
 
-                System.out.println("------------ Constructor");
-                
-                ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean ("constructorInjectedController");
-               
-                System.out.println(constructorInjectedController.getGreeting());
+        PetController petController = ctx.getBean("petController", PetController.class);
+        System.out.println("--- The Best Pet is ---");
+        System.out.println(petController.whichPetIsTheBest());
 
-            FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
-            System.out.println(fakeDataSource.getUsername());
-            System.out.println(fakeDataSource.getPassword());
-            System.out.println(fakeDataSource.getJdbcurl());
+        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+        System.out.println(i18nController.sayHello());
+
+        System.out.println("------------ Primary");
+
+        MyController myController = (MyController) ctx.getBean("myController");
+
+        System.out.println(myController.sayHello());
+
+        System.out.println("------------ Property");
+
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+
+        System.out.println(propertyInjectedController.getGreeting());
+
+        System.out.println("------------ Setter");
+
+        SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+
+        System.out.println(setterInjectedController.getGreeting());
+
+        System.out.println("------------ Constructor");
+
+        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+
+        System.out.println(constructorInjectedController.getGreeting());
+
+        FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUsername());
+        System.out.println(fakeDataSource.getPassword());
+        System.out.println(fakeDataSource.getJdbcurl());
+
+        System.out.println("--------------------------Config Props Bean");
+        SfgConfiguration sfgConfiguration = ctx.getBean(SfgConfiguration.class);
+        System.out.println(sfgConfiguration.getUsername());
+        System.out.println(sfgConfiguration.getPassword());
+        System.out.println(sfgConfiguration.getJdbcurl());
+
     }
 }
